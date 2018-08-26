7049e6a2a2a631eaa51a1ded2732afffffaf11e2
brianhouse <brian.house@gmail.com>
2018-08-26 19:40:55 +0200

    changed to toneAC, implemented an LFO

--- acid_love.ino
+++ acid_love.ino
@@ -1,7 +1,11 @@
-const int SPEAKER = A0;
-unsigned long DURATION = 1; // in minutes
-const int RANGE_LOW = 40;
-const int RANGE_HIGH = 100;
+#include <toneAC.h>
+#define SPEAKER A0
+#define RANGE_LOW 40
+#define RANGE_HIGH 100
+
+unsigned long DURATION = 1;    // in minutes
+unsigned long LFO = 20;        // in seconds
+
 
 int data[] = {
   0, 20, 100, 40, 50, 20, 10, 30, 40, 90
@@ -9,7 +13,6 @@ int data[] = {
 int data_length = sizeof(data) / sizeof(data[0]);
 
 void setup() {
-  pinMode(SPEAKER, OUTPUT);  
   Serial.begin(115200);
   Serial.println("Hello world");
   Serial.print("Size of data: ");
@@ -18,11 +21,14 @@ void setup() {
   Serial.print(DURATION);
   Serial.println("min");
   DURATION *= 60000;
+  LFO *= 1000;
 }
 
 void loop() {
 
-  float pos = (millis() % DURATION) / float(DURATION); // position 0-1 in data
+  unsigned long m = millis();
+  
+  float pos = (m % DURATION) / float(DURATION);        // position 0-1 in data
   pos *= data_length;                                  // position in the array
   int i = pos;                                         // floor index of that position
   pos -= i;                                            // position between indexes
@@ -30,18 +36,26 @@ void loop() {
   int pitch_1 = (int)((data[i] / 100.0) * (RANGE_HIGH - RANGE_LOW)) + RANGE_LOW;  
   int pitch_2 = (int)((data[(i + 1) % data_length] / 100.0) * (RANGE_HIGH - RANGE_LOW)) + RANGE_LOW;
   int pitch = pitch_1 + ((pitch_2 - pitch_1) * pos);
-//
-//  Serial.print(i);
-//  Serial.print(" ");
-//  Serial.print(pos);
-//  Serial.print(" ");
-//  Serial.print(pitch_1);
-//  Serial.print(" ");
-//  Serial.print(pitch_2);
-//  Serial.print(" > ");
-//  Serial.print(pitch);
-//  Serial.println();
 
-  tone(SPEAKER, pitch);
+  float volume = (m % LFO) / float(LFO);
+  volume *= 6;
+  volume += 5;
+  
+  toneAC(pitch, volume);
+
+  Serial.print(i);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.print(" ");
+  Serial.print(pitch_1);
+  Serial.print(" ");
+  Serial.print(pitch_2);
+  Serial.print(" > ");
+  Serial.print(pitch);
+  Serial.print(" @ ");
+  Serial.print(volume);  
+  Serial.println();
+
+  delay(1);
       
 }
