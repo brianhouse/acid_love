4d501e6f401976571629e1d02e7ebaeea2ba42d4
brianhouse <brian.house@gmail.com>
2018-08-26 18:43:00 +0200

    fixed overflow issues

--- acid_love.ino
+++ acid_love.ino
@@ -1,15 +1,12 @@
-const int SPEAKER = A0;       // A0
-const int DURATION = 10;      // in seconds
+const int SPEAKER = A0;
+unsigned long DURATION = 1; // in minutes
 const int RANGE_LOW = 40;
-const int RANGE_HIGH = 200;
-
-float elapsed = 0.0;
-int p_t = 0;
-int p_s = 0;
+const int RANGE_HIGH = 100;
 
 int data[] = {
   0, 20, 100, 40, 50, 20, 10, 30, 40, 90
 };
+int data_length = sizeof(data) / sizeof(data[0]);
 
 void setup() {
   pinMode(SPEAKER, OUTPUT);  
@@ -17,30 +14,34 @@ void setup() {
   Serial.println("Hello world");
   Serial.print("Size of data: ");
   Serial.println(sizeof(data));
+  Serial.print("DURATION: ");
+  Serial.print(DURATION);
+  Serial.println("min");
+  DURATION *= 60000;
 }
 
 void loop() {
 
-  // update timers
-  int t = millis();
-  elapsed += t - p_t;
-  if (elapsed >= DURATION * 1000) elapsed = 0;
-  float pos = elapsed / (DURATION * 1000);
-  int s = elapsed / 1000;
-
-  int i = pos * (sizeof(data) / sizeof(data[0]));
-  int pitch = (int)((data[i] / 100.0) * (RANGE_HIGH - RANGE_LOW)) + RANGE_LOW;
+  float pos = (millis() % DURATION) / float(DURATION); // position 0-1 in data
+  pos *= data_length;                                  // position in the array
+  int i = pos;                                         // floor index of that position
+  pos -= i;                                            // position between indexes
+  
+  int pitch_1 = (int)((data[i] / 100.0) * (RANGE_HIGH - RANGE_LOW)) + RANGE_LOW;  
+  int pitch_2 = (int)((data[(i + 1) % data_length] / 100.0) * (RANGE_HIGH - RANGE_LOW)) + RANGE_LOW;
+  int pitch = pitch_1 + ((pitch_2 - pitch_1) * pos);
+//
+//  Serial.print(i);
+//  Serial.print(" ");
+//  Serial.print(pos);
+//  Serial.print(" ");
+//  Serial.print(pitch_1);
+//  Serial.print(" ");
+//  Serial.print(pitch_2);
+//  Serial.print(" > ");
+//  Serial.print(pitch);
+//  Serial.println();
 
   tone(SPEAKER, pitch);
-    
-  if (s != p_s) {
-    Serial.println(i);
-    Serial.println(pitch);
-    Serial.println("");
-  }
-
-  // update timers
-  p_s = s;
-  p_t = t;
-  
+      
 }
